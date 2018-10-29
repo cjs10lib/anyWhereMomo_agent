7fe3f92a4f158d9d1ad8eef85508ad251558b3ed
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2018-10-29 12:16:42 +0000

    Extracted login form to component

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage, LoginFormComponent]
 })
 export class LoginPageModule {}
