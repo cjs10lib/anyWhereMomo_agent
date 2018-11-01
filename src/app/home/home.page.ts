9e35f7c98af88bb54cad3361e02b5334ceb2b773
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2018-11-01 16:31:21 +0000

    Added geolocation with capacitor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,31 +1,48 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Plugins } from '@capacitor/core';
 import * as leaflet from 'leaflet';
+
+const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   @ViewChild('map') mapContainer: ElementRef;
-  map: any;
+  map: leaflet.Map;
 
   constructor(private router: Router) { }
 
+  ngOnInit() { }
+
   ionViewDidEnter() {
     if (!this.map) {
-      this.loadmap();
+      this.getCurrentPosition();
     }
+
   }
 
-  loadmap() {
-    this.map = leaflet.map('map').fitWorld();
-    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attributions: 'Cybotech Coorp',
-      maxZoom: 20
-    }).addTo(this.map);
+  async getCurrentPosition() {
+    Geolocation.getCurrentPosition().then(coords => {
+
+      this.map = leaflet.map('map').setView([coords.coords.latitude, coords.coords.longitude], 15);
+      leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attributions: 'Cybotech Coorp',
+      }).addTo(this.map);
+
+      const markerGroup = leaflet.featureGroup();
+      const marker: any = leaflet.marker([coords.coords.latitude, coords.coords.longitude]);
+
+      markerGroup.addLayer(marker)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+      this.map.addLayer(markerGroup);
+    });
   }
 
   navigateToProfilePage() {
