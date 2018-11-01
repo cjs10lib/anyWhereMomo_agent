23acaac85db468039447642d4b439fa6745053b8
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2018-11-01 12:37:15 +0000

    Installed leaflet map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import * as leaflet from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,24 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
+
   constructor(private router: Router) { }
+
+  ionViewDidEnter() {
+    if (!this.map) {
+      this.loadmap();
+    }
+  }
+
+  loadmap() {
+    this.map = leaflet.map('map').fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'Cybotech Coorp',
+      maxZoom: 10
+    }).addTo(this.map);
+  }
 
   navigateToProfilePage() {
     this.router.navigate(['profile']);
