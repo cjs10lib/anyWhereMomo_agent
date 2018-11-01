2dd32feab66ab903692cff136d0930ba4a8ac33c
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2018-11-01 17:26:09 +0000

    Extracting map to component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,5 @@
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { Plugins } from '@capacitor/core';
-import * as leaflet from 'leaflet';
-
-const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -12,38 +8,9 @@ const { Geolocation } = Plugins;
 })
 export class HomePage implements OnInit {
 
-  @ViewChild('map') mapContainer: ElementRef;
-  map: leaflet.Map;
-
   constructor(private router: Router) { }
 
   ngOnInit() { }
-
-  ionViewDidEnter() {
-    if (!this.map) {
-      this.getCurrentPosition();
-    }
-
-  }
-
-  async getCurrentPosition() {
-    Geolocation.getCurrentPosition().then(coords => {
-
-      this.map = leaflet.map('map').setView([coords.coords.latitude, coords.coords.longitude], 15);
-      leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attributions: 'Cybotech Coorp',
-      }).addTo(this.map);
-
-      const markerGroup = leaflet.featureGroup();
-      const marker: any = leaflet.marker([coords.coords.latitude, coords.coords.longitude]);
-
-      markerGroup.addLayer(marker)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-
-      this.map.addLayer(markerGroup);
-    });
-  }
 
   navigateToProfilePage() {
     this.router.navigate(['profile']);
---
+++ src/app/home/map/map.component.ts
@@ -0,0 +1,53 @@
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { GeolocationPosition, Plugins } from '@capacitor/core';
+import * as leaflet from 'leaflet';
+
+
+const { Geolocation } = Plugins;
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('map') mapContainer: ElementRef;
+  map: leaflet.Map;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    if (!this.map) {
+      this.getCurrentPosition();
+    }
+  }
+
+  initializeMap(position: GeolocationPosition) {
+    this.map = leaflet.map('map').setView([position.coords.latitude, position.coords.longitude], 16);
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'Cybotech Coorp',
+    }).addTo(this.map);
+  }
+
+  addMarker(position: GeolocationPosition) {
+    const markerGroup = leaflet.featureGroup();
+    const marker: any = leaflet.marker([position.coords.latitude, position.coords.longitude]);
+
+    markerGroup.addLayer(marker)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+    this.map.addLayer(markerGroup);
+  }
+
+  async getCurrentPosition() {
+    const position = await Geolocation.getCurrentPosition();
+
+    this.initializeMap(position);
+    this.addMarker(position);
+  }
+
+}
